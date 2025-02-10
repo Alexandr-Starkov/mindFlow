c03239403de991fda305ce585e1db7320248d3c4
Alexandr-Starkov <Starkoff55@gmail.com>
2025-02-10 14:45:53 +0600

    Добавлена модель Task, модель подключена в admin.py, выполнена init миграция.

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = 'pk', 'title', 'description_short', 'created_at', 'updated_at', 'is_completed'
+    list_display_links = 'pk', 'title'
+
+    def description_short(self, obj: Task) -> str:
+        if len(obj.description) < 48:
+            return obj.description
+        return obj.description[:48] + '...'
+
+    description_short.short_description = 'Short Description'
